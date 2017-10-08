2017c6696b7bc4df10ba0564d8b7206cefdad6fe
jruimouta <1161313@isep.ipp.pt>
2017-10-09 00:06:50 +0100

    exercicios

--- modulo1/ex20/find_all_words.c
+++ modulo1/ex20/find_all_words.c
@@ -5,22 +5,20 @@
 
 void find_all_words(char* str, char* word, char** addrs)
 {
-	printf("XX");
 	char *initial_addr;
 	initial_addr=str;
-	
-	printf("\n x-> %c",*initial_addr);
-	
-	
+		
 	while(initial_addr!=NULL){
 
 		initial_addr=find_word(str,word,initial_addr);
-		printf("\n xxxxxxxx-%p",initial_addr);
-		*addrs=initial_addr;
-		addrs++;
-		initial_addr++;
 		if(initial_addr==NULL){
 			break;
 		}
+		printf("\n endereco -> %p",initial_addr);
+		*addrs=initial_addr;
+		addrs++;
+
+		initial_addr++;
+
 	}
 }
